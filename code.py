ad541e8c2aca6ffd59b4243ccd95f66c2e4f8c34
LIGNEL Pierre <Pierre.Lignel@bordeaux-inp.fr>
2025-11-21 10:16:00 +0100

    apply construit

--- code.py
+++ code.py
@@ -2,3 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.ndimage import convolve
 from skimage import io, color, feature
+
+
+def homography_apply(H, x1, y1):
+    x2 = H[0][0]* x1 + H[0][1]* y1 + H[0][2]/(H[2][0]* x1 + H[2][1]* y1 + H[2][2])
+    y2 = H[1][0]* x1 + H[1][1]* y1 + H[1][2]/(H[2][0]* x1 + H[2][1]* y1 + H[2][2])
+
+    return x2, y2
