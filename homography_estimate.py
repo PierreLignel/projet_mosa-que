01e45b2750477c1aa74be8ee560c402e1d8b5fb7
CAZAUX Aymeric <Aymeric.Cazaux@bordeaux-inp.fr>
2025-11-21 11:20:07 +0100

    homography_estimate

---
+++ homography_estimate.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def homography_estimate(x1, y1, x2, y2):
+    
+    A = np.zeros((8, 8))
+    B = np.zeros((8, 1))
+    H = np.zeros((3, 3))
+    
+    for i in range(4):
+        A[2*i][0] = x1[i]
+        A[2*i][1] = y1[i]
+        A[2*i][2] = 1;
+        A[2*i][6] = -(x1[i]*x2[i])
+        A[2*i][7] = -(y1[i]*x2[i])
+        A[(2*i)+1][3] = x1[i]
+        A[(2*i)+1][4] = y1[i]
+        A[(2*i)+1][5] = 1
+        A[(2*i)+1][6] = -(x1[i]*y2[i])
+        A[(2*i)+1][7] = -(y1[i]*y2[i])
+    
+    for i in range(4):
+        B[2*i] = x2[i]
+        B[(2*i)+1] = y2[i]
+        
+    X = np.linalg.solve(A, B)
+    
+    H[0][0] = X[0]
+    H[0][1] = X[1]
+    H[0][2] = X[2]
+    H[1][0] = X[3]
+    H[1][1] = X[4]
+    H[1][2] = X[5]
+    H[2][0] = X[6]
+    H[2][1] = X[7]
+    H[2][2] = 1
+    
+    return H
+    
+    
+        
+    
