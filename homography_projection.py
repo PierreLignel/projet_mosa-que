b4e21d57ace026fdff8759b1efb069e2a2fc8e7b
LIGNEL Pierre <Pierre.Lignel@bordeaux-inp.fr>
2025-11-28 12:16:47 +0100

    chgmt projection

--- homography_projection.py
+++ homography_projection.py
@@ -1,4 +1,5 @@
 
+ 
 def homography_projection(I1, I2, x, y):
     h, w = I1.shape[:2]
     x1 = [0, w-1, w-1, 0]
@@ -6,14 +7,14 @@ def homography_projection(I1, I2, x, y):
     
     H = homography_estimate( x, y,x1, y1)
 
-    for i in range(I2.shape[0]):
-        for j in range(I2.shape[1]):
-            x2, y2 = homography_apply(H, i, j)
+    for yy in range(I2.shape[0]):
+        for xx in range(I2.shape[1]):
+            x2, y2 = homography_apply(H, xx, yy)
             x2 = int(round(x2))
             y2 = int(round(y2))
             
             if 0 <= x2 < w and 0 <= y2 < h:
-                I2[j][i] = I1[y2][x2]
-    
-    return I1
+                I2[yy][xx] = I1[y2][x2]
 
+    return I2
+ 
